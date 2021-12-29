aa4f3522b9847843b370a8cb682985e8e931658e
yuanjie <zhuyuanjie1992@126.com>
2021-12-29 17:41:10 +0800

    feat: 添加publish

---
+++ src/screen/publish/quest-publish/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const QuestPublishScreen: React.FC = (props) => {
+    return (
+        <View>
+            <Text>QuestPublishScreen</Text>
+        </View>
+    );
+};
+
+export default QuestPublishScreen;
